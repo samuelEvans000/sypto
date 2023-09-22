9fff27a09bd970c9b78f53d56036ab010b59aa89
samuelEvans000 <chintuvincent@gmail.com>
2023-09-23 02:23:51 +0530

    updated

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -12,22 +12,22 @@ function Navbar() {
       <nav className="navbar-content">
       <ul className="nav-list">
         <li className="nav-item">
-          <a href="/">Products</a>
+          <a >Products</a>
         </li>
         <li className="nav-item">
-          <a href="/about">Services</a>
+          <a >Services</a>
         </li>
         <li className="nav-item">
-          <a href="/services">Blog</a>
+          <a >Blog</a>
         </li>
         <li className="nav-item">
-          <a href="/services">About Us</a>
+          <a >About Us</a>
         </li>
         <li className="nav-item">
-          <a href="/contact">Contact</a>
+          <a >Contact</a>
         </li>
         <li className="nav-item">
-          <a href="/services">Login</a>
+          <a >Login</a>
         </li>
       </ul>
     </nav>
